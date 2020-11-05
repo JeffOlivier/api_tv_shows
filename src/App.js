3f73629ebac4efd7d9bb09e175aee37bde5d563f
Jeff Olivier <jeff@jeffolivier.com>
2020-11-05 10:31:30 -0500

    Cleanup

--- src/App.js
+++ src/App.js
@@ -1,60 +1,77 @@
-import React from "react";
-import FetchShows from "./components/fetchShows";
+import React, { Component } from "react";
+// import FetchShows from "./components/fetchShows";
 
 import "./App.css";
 
-class App extends React.Component { 
+class App extends Component { 
   state = {
     hasSearchedBefore: false,
-    loading: true,
     searchTerm: '',
-    shows: {}
+    loading: false,
+    shows: []
   };
 
+  async fetchResults(searchTerm1) {
+    const searchTerm = searchTerm1.trim();
+    const apiFullUrl = `https://api.tvmaze.com/search/shows?q=${searchTerm}`;
 
-  handleFetchResults(shows) {
-    this.setState({shows: shows});
-    console.log(shows);
-  }
-
-  onWantSearch(searchTerm) {
-    console.log('CALLED onWantSearch()');
-    if (searchTerm != null) {
-      this.setState({searchTerm: searchTerm});
-
-      console.log('searchTerm: '+searchTerm);
-      console.log(this.state);
-    } 
-    else {console.log('NO SEARCH TERM');}
+    if (searchTerm == null) return;
+    
+    this.setState({ loading: true });
+    
+    await fetch(apiFullUrl)
+        .then(response => response.json())
+        .then(response => this.setState({ shows: response, hasSearchedBefore: true, loading: false, searchTerm: searchTerm }))
+        .catch(err => { console.log(err); });
   }
 
   render() {
+    let foundShowsOutput;
+    if (this.state.hasSearchedBefore && this.state.shows.length === 0) {
+      foundShowsOutput = <div>Could not find any shows matching "<strong>{this.state.searchTerm}</strong>"</div>
+    } else {
+      foundShowsOutput = this.state.shows.map((show) => (
+        <SingleShow {...show.show}/>
+      ))
+    }
+
     return (
       <React.Fragment>
 
         <div className="App">
-          <header className="App-header">
-            <h2>Find TV Shows</h2>
-            
-            Enter a term to search for: <input id="findSearchTerm" type="text" required />
-
-            <button className="btn btn-primary" type='button' onClick={() => this.onWantSearch(document.getElementById('findSearchTerm').value)}>SEARCH</button>
+          <header className="App-header2">
+            <div className="fubar">
+            <span className="fas fa-search fa-2x"></span>
+            <input id="findSearchTerm" className="input_searchterm" type="text" placeholder="Enter search term" required />
+            <button className="btn btn_search" type='button' onClick={() => this.fetchResults(document.getElementById('findSearchTerm').value)}>SEARCH</button>
+            </div>
           </header>
         </div>
 
         <main className="container">
-          <FetchShows
-            searchTerm={this.state.searchTerm}
-            onFetch={this.handleFetchResults}
-          />
+          <div id="listOfShows" className="results_block">
+            {foundShowsOutput}
+          </div>
         </main>
       </React.Fragment>
     );
   }
 
-  formatRating(rating) {
-    return rating === null ? '--' : rating;
-  }
+}
+
+
+const SingleShow = (props) => {
+  return (
+    <div class="showBlock">
+      <img src={props.image.medium} alt='show pic'/>
+      <div class="showInfo">
+        <h1>{props.name}</h1>
+        <div dangerouslySetInnerHTML={{__html: props.summary}}></div>
+        <a href={props.url} target="new">
+          <button class="btn btn_episodes" type='button'>More about this show</button>
+        </a>
+      </div>
+    </div>);
 }
 
 export default App;
