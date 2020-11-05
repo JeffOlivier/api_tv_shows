bb1832d1013fa89181b7dba324ac979692980445
Jeff Olivier <jeff@jeffolivier.com>
2020-11-05 17:01:19 -0500

    Only show more link when the URL exists

--- src/components/singleShow.jsx
+++ src/components/singleShow.jsx
@@ -5,7 +5,9 @@ const SingleShow = (props) => {
     const imageUrl = ((props.image != null) && (props.image.medium != null)) ? props.image.medium : './no-image.png';
     const title = (props.name != null) ? props.name : 'Untitled';
     const summary = (props.summary != null) ? props.summary : '';
-    const moreLink = (props.url != null) ? props.url : '';
+    const moreLink = (props.url != null) ? 
+        `<a href=${props.url} target="new"><button class="btn ${styles.btn_episodes}" type='button'>More about this show</button></a>` 
+    : '';
 
     return (
         <div className={styles.showBlock}>
@@ -13,9 +15,7 @@ const SingleShow = (props) => {
             <div className={styles.showInfo}>
                 <h1>{title}</h1>
                 <div dangerouslySetInnerHTML={{__html: summary}}></div>
-                <a href={moreLink} target="new">
-                    <button className={`btn ${styles.btn_episodes}`} type='button'>More about this show</button>
-                </a>
+                <div dangerouslySetInnerHTML={{__html: moreLink}}></div>
             </div>
         </div>
     );
