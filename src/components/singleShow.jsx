01b04ff10f2a0e10dd57d11e913b3acf2884946a
Jeff Olivier <jeff@jeffolivier.com>
2020-11-05 16:51:30 -0500

    Validates returned show info

--- src/components/singleShow.jsx
+++ src/components/singleShow.jsx
@@ -2,13 +2,18 @@ import React from 'react';
 import styles from './singleShow.module.css';
 
 const SingleShow = (props) => {
+    const imageUrl = ((props.image != null) && (props.image.medium != null)) ? props.image.medium : './no-image.png';
+    const title = (props.name != null) ? props.name : 'Untitled';
+    const summary = (props.summary != null) ? props.summary : '';
+    const moreLink = (props.url != null) ? props.url : '';
+
     return (
         <div className={styles.showBlock}>
-            <img src={props.image.medium} alt='show pic'/>
+            <img src={imageUrl} alt='show pic'/>
             <div className={styles.showInfo}>
-                <h1>{props.name}</h1>
-                <div dangerouslySetInnerHTML={{__html: props.summary}}></div>
-                <a href={props.url} target="new">
+                <h1>{title}</h1>
+                <div dangerouslySetInnerHTML={{__html: summary}}></div>
+                <a href={moreLink} target="new">
                     <button className={`btn ${styles.btn_episodes}`} type='button'>More about this show</button>
                 </a>
             </div>
