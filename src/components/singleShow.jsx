4f12153c03c741ab62b82ff77b8b4ccad83e81e1
Jeff Olivier <jeff@jeffolivier.com>
2020-11-05 14:05:18 -0500

    Separates the CSS into a file

--- src/components/singleShow.jsx
+++ src/components/singleShow.jsx
@@ -1,16 +1,17 @@
 import React from 'react';
+import styles from './singleShow.module.css';
 
 const SingleShow = (props) => {
     return (
-        <div class="showBlock">
-        <img src={props.image.medium} alt='show pic'/>
-        <div class="showInfo">
-            <h1>{props.name}</h1>
-            <div dangerouslySetInnerHTML={{__html: props.summary}}></div>
-            <a href={props.url} target="new">
-            <button class="btn btn_episodes" type='button'>More about this show</button>
-            </a>
-        </div>
+        <div className={styles.showBlock}>
+            <img src={props.image.medium} alt='show pic'/>
+            <div className={styles.showInfo}>
+                <h1>{props.name}</h1>
+                <div dangerouslySetInnerHTML={{__html: props.summary}}></div>
+                <a href={props.url} target="new">
+                    <button className={`btn ${styles.btn_episodes}`} type='button'>More about this show</button>
+                </a>
+            </div>
         </div>
     );
 }
