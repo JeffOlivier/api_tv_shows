898d8161d888f3be4dcae76b48759f8d492bf3d9
Jeff Olivier <jeff@jeffolivier.com>
2020-11-06 15:48:00 -0500

    Adds a form validator

--- src/App.js
+++ src/App.js
@@ -4,18 +4,33 @@ import SingleShow from "./components/singleShow";
 import "./App.css";
 
 class App extends Component { 
-  state = {
-    hasSearchedBefore: false,
-    searchTerm: '',
-    loading: false,
-    shows: []
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      hasSearchedBefore: false,
+      searchTerm: '',
+      loading: false,
+      shows: [],
+      isSearchFormValid: true
+    };
+
+    this.handleValidateInput = this.handleValidateInput.bind(this);
+  }
+
+  handleValidateInput = e => {
+    const validChars = /^[a-zA-Z0-9]+$/i;
+    const searchInput = e.target.value;
+
+    this.setState({ isSearchFormValid: validChars.test(searchInput) });
+
+    document.getElementById('searchBtn').disabled = !(this.state.isSearchFormValid);
+  }
 
   async fetchResults(searchTerm1) {
     const searchTerm = searchTerm1.trim();
     const apiFullUrl = `https://api.tvmaze.com/search/shows?q=${searchTerm}`;
 
-    if (searchTerm == null) return;
+    if ((searchTerm == null) || (searchTerm === '')) return;
     
     this.setState({ loading: true });
     
@@ -28,14 +43,14 @@ class App extends Component {
   render() {
     let foundShowsOutput;
     if (!this.state.hasSearchedBefore) {
-      foundShowsOutput = <div class="centerMe">Use the search field above to find TV shows</div> 
+      foundShowsOutput = <div className="centerMe">Use the search field above to find TV shows</div> 
     // } else if (this.state.loading) {
-    //   return <div class="centerMe">finding TV shows ...</div>
+    //   return <div className="centerMe">finding TV shows ...</div>
     } else if (this.state.hasSearchedBefore && this.state.shows.length === 0) {
-      foundShowsOutput = <div class="centerMe">Could not find any shows matching "<strong>{this.state.searchTerm}</strong>"</div>
+      foundShowsOutput = <div className="centerMe">Could not find any shows matching "<strong>{this.state.searchTerm}</strong>"</div>
     } else {
       foundShowsOutput = this.state.shows.map((show) => (
-        <SingleShow {...show.show}/>
+        <SingleShow {...show.show} key={show.show.id}/>
       ))
     }
 
@@ -45,8 +60,8 @@ class App extends Component {
         <div className="App">
           <header>
             <span className="fas fa-search fa-2x"></span>
-            <input id="findSearchTerm" className="input_searchterm" type="text" placeholder="Enter search term" required />
-            <button className="btn btn_search" type='button' onClick={() => this.fetchResults(document.getElementById('findSearchTerm').value)}>SEARCH</button>
+            <input id="findSearchTerm" className="input_searchterm" type="text" onKeyUp={this.handleValidateInput} placeholder="Enter search term" required />
+            <button id="searchBtn" className="btn btn_search" type='button' onClick={() => this.fetchResults(document.getElementById('findSearchTerm').value)}>SEARCH</button>
           </header>
         </div>
 
