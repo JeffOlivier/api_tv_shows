9713d0acdd600f77e37dcb8947c34e06605b80b4
Jeff Olivier <jeff@jeffolivier.com>
2020-11-24 22:11:12 -0500

    Changes filenames to capitals pt2

---
+++ src/components/SingleShow.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import styles from './SingleShow.module.scss';
+
+const SingleShow = (props) => {
+    const imageUrl = ((props.image != null) && (props.image.medium != null)) ? props.image.medium : './no-image.png';
+    const title = (props.name != null) ? props.name : 'Untitled';
+    const summary = (props.summary != null) ? props.summary : '';
+    // const moreLink = (props.url != null) ? 
+    //     `<a href=${props.url} target="new"><button class="btn ${styles.btn_episodes}" type='button'>More about this show</button></a>` 
+    // : '';
+
+    const episodesButtonClasses = `btn ${styles.btn_episodes}`;
+
+    return (
+        <div className={styles.showBlock}>
+            <img src={imageUrl} alt='show pic'/>
+            <div className={styles.showInfo}>
+                <h1>{title}</h1>
+                <div dangerouslySetInnerHTML={{__html: summary}}></div>
+                {/* <div dangerouslySetInnerHTML={{__html: moreLink}}></div> */}
+                
+                <button className={episodesButtonClasses} onClick={() => props.updateShowId(props.id)} type='button'>Show Episodes</button>
+                {/* className={episodesButtonClasses} */}
+            </div>
+        </div>
+    );
+}
+
+export default SingleShow;
